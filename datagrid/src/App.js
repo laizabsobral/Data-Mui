38e4f4a5b80c80c397c8788a4062288a5342ca5d
laiza <laizabsobral@gmail.com>
2023-08-27 11:13:29 -0300

    upd datagrid and datatable

--- datagrid/src/App.js
+++ datagrid/src/App.js
@@ -1,10 +1,23 @@
+import * as React from 'react';
+import { DataGrid} from '@mui/x-data-grid';
 
-import './App.css';
 
 function App() {
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70},
+    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'age', headerName: 'Age', width: 70},
+  ];
+
+  const rows = [
+    { id: 1, name: 'Ana', age: '25'},
+    { id: 2, name: 'Joana', age: '17'},
+    { id: 2, name: 'Daniela', age: '42'},
+  ];
+ 
   return (
-    <div >
-     
+    <div style={{height: 300, width: '100%'}}>
+        <DataGrid columns={columns} rows={rows}/>
     </div>
   );
 }
--- datatable/src/App.js
+++ datatable/src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import { TableContainer, Table, TableRow, TableCell, TableBody, TableHead} from '@mui/material';
+
+const data = [
+  { id: 1, name: 'Ana', age: '25'},
+  { id: 2, name: 'Joana', age: '17'},
+  { id: 2, name: 'Daniela', age: '42'},
+];
+
 function App() {
   return (
-   <div>
-    
-   </div>
+   <TableContainer>
+      <Table>
+         <TableHead>
+            <TableCell> ID </TableCell>
+            <TableCell> Nome </TableCell>
+            <TableCell> Idade </TableCell>
+         </TableHead>
+
+         <TableBody>
+            {data.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell> {row.id} </TableCell>
+                <TableCell> {row.name}</TableCell>
+                <TableCell> {row.age} </TableCell>
+              </TableRow>
+            ))}
+         </TableBody>
+      </Table>
+   </TableContainer>
   );
 }
 
